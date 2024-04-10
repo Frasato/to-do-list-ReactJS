de43450f431c7d4e70cacbce6caf9bebfbebd87b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-04-10 20:51:12 -0300

    create function createTask and show on the screen

--- src/App.js
+++ src/App.js
@@ -15,15 +15,26 @@ export default function App() {
     setDescription(event.target.value);
   }
 
+  function createTask(event){
+    event.preventDefault();
+    const taskObj = [
+      {
+        name: name,
+        desc: description
+      }
+    ];
+    setTasks([ ...tasks, ...taskObj]);
+  }
+
   return (
     <div>
       <h1>To Dev List</h1>
       <input type="text" placeholder="Task name..." value={name} onChange={handleNameTask}/>
       <textarea value={description} onChange={handleDescription}></textarea>
-      <button>Create</button>
-      {tasks.map((itemTask, id)=>{
+      <button onClick={createTask}>Create</button>
+      {tasks.length === 0? <span>Don't have any tasks...</span> : tasks.map((itemTask, id)=>{
         return(
-          <Task id={id} nameTask={itemTask.name} description={itemTask.description}/>
+          <Task id={id} nameTask={itemTask.name} description={itemTask.desc}/>
         );
       })}
     </div>
